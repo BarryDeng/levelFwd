d100b6ba182a4469b030d2390433cfa852da48e9
wuweinan <573877858@qq.com>
2018-05-01 16:43:27 +0800

    Level add mac

--- src/main/java/edu/nuaa/levelFwd/Level.java
+++ src/main/java/edu/nuaa/levelFwd/Level.java
@@ -1,25 +1,34 @@
 package edu.nuaa.levelFwd;
 
+
+import org.onlab.packet.MacAddress;
+
 public enum Level {
-    WHITELIST(10000,(short)1),
-    RELIABLE(10001, (short)2),
-    NORMAL(10002, (short)3),
-    THREAT(10003, (short)4),
-    BLACKLIST(10004, (short)5);
+    WHITELIST(10000,(short)1, ""),
+    RELIABLE(10001, (short)2, ""),
+    NORMAL(10002, (short)4, ""),
+    THREAT(10003, (short)5, ""),
+    BLACKLIST(10004, (short)6, "");
 
     private int code;
     private short port;
-    private Level(int code, short port) {
+    private String mac;
+    private Level(int code, short port, String mac) {
         this.code = code;
         this.port = port;
+        this.mac = mac;
     }
 
     public int getCode() {
-        return code;
+        return this.code;
     }
 
     public short getPort() {
-        return port;
+        return this.port;
+    }
+
+    public MacAddress getMac(){
+        return MacAddress.valueOf(this.mac);
     }
 
     public void setCode(int code) {
@@ -28,6 +37,10 @@ public enum Level {
 
     public void setPort(short port) {
         this.port = port;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
     }
 
     public static Level getByValue(int value) {
